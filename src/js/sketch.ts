5ee28e6f63cc3c17037a292d031b41b8484a3e41
ryan.delbango@gmail.com <ryan.delbango@gmail.com>
2022-06-05 23:08:16 -0400

    Add: p5 boiler plate template

---
+++ src/js/sketch.ts
@@ -0,0 +1,43 @@
+import p5 from 'p5';
+import '../css/style.scss';
+
+const sketch = (p: p5) => {
+  let logo: p5.Image;
+  let logoWidth = 250;
+  let logoHeight = 114;
+
+  p.preload = () => {
+    logo = p.loadImage('assets/p5js.svg');
+  };
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.image(
+      logo,
+      p.windowWidth / 2 - logoWidth / 2,
+      p.windowHeight / 2 - logoHeight / 2
+    );
+  };
+
+  p.draw = () => {
+    if (p.mouseIsPressed) {
+      p.fill(0);
+    } else {
+      p.fill(255);
+    }
+    p.ellipse(p.mouseX, p.mouseY, 80, 80);
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+    p.image(
+      logo,
+      p.windowWidth / 2 - logoWidth / 2,
+      p.windowHeight / 2 - logoHeight / 2
+    );
+  };
+
+  p.keyPressed = () => {};
+};
+
+new p5(sketch);
