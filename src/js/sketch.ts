de79895f5b05b64dcdc172c6e5518c16cc4c238b
ryan.delbango@gmail.com <ryan.delbango@gmail.com>
2022-07-09 14:28:37 -0400

    add: growing bunny

--- src/js/sketch.ts
+++ src/js/sketch.ts
@@ -1,13 +1,21 @@
 import p5 from 'p5';
 import '../css/style.scss';
 
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 const sketch = (p: p5) => {
   let logo: p5.Image;
+  let img: p5.Image;
   let logoWidth = 250;
   let logoHeight = 114;
 
   p.preload = () => {
     logo = p.loadImage('assets/p5js.svg');
+    img = p.loadImage('assets/rabbit.png');
   };
 
   p.setup = () => {
@@ -17,25 +25,23 @@ const sketch = (p: p5) => {
   let width = initial;
   let height = initial;
   const increment = 30;
+  let x = getRandomInt(0, p.windowWidth);
+  let y = getRandomInt(0, p.windowHeight);
   p.draw = () => {
-    p.rect(initial, initial, width, height)
+    p.image(img, x, y, width, height)
     p.mouseWheel = (event: any) => {
-      p.clear(0, 0, 0, 0);
-      if (event?.delta < 0) {
-        if (width < p.windowWidth - (initial + 10)) {
-          width += increment;
-        };
-        if (height < p.windowHeight - (initial + 10)) {
-          height += increment;
-        };
-      } else if (event?.delta > 0) {
-        if (width > initial) {
-          width -= increment;
-        }
-        if (height > initial) {
-          height -= increment;
-        }
+      x = getRandomInt(0, p.windowWidth);
+      y = getRandomInt(0, p.windowHeight);
+      if (event?.delta > 0) {
+        width += increment;
+        height += increment;
+      } else if (event?.delta < 0) {
+        width -= increment;
+        height -= increment;
       }
+    };
+    if (p.keyIsPressed) {
+      p.clear(0,0,0,0);
     }
   };
 
